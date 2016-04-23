78fbbf9e63f849bfe1360c83fe49206ef6122ab8
devholic <devholic@plusquare.com>
2016-04-24 01:05:11 +0900

    expvar: change string to interface{} for various type support

--- expvar.go
+++ expvar.go
@@ -2,10 +2,10 @@ package goniplus
 
 import "expvar"
 
-func getExpvar() map[string]string {
-	m := make(map[string]string)
+func getExpvar() map[string]interface{} {
+	m := make(map[string]interface{})
 	expvar.Do(func(kv expvar.KeyValue) {
-		m[kv.Key] = kv.Value.String()
+		m[kv.Key] = kv.Value
 	})
 	return m
 }
