c774d3aaf204e50428caf7fe62b8d42b455bcb73
devholic <devholic@plusquare.com>
2016-04-25 01:05:42 +0900

    metric: add sender.go for send metric data to server

---
+++ sender.go
@@ -0,0 +1,35 @@
+package goniplus
+
+import (
+	"log"
+	"net"
+	"time"
+)
+
+// sendMetricData(data []byte) returns true if data successfully sent
+func sendMetricData(data []byte) bool {
+	conn, err := net.Dial("tcp", metricURL)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	defer conn.Close()
+	if _, err = conn.Write(data); err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
+
+// startSender starts timer for sending metrics to server
+// every `sendInterval` second
+func (c *Client) startSender() {
+	for range time.Tick(c.sendInterval) {
+		data, err := getMetric()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go sendMetricData(data)
+	}
+}
