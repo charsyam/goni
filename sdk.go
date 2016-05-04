69584a6b758ce907923f85856694bbbdf9df05a1
devholic <devholic@plusquare.com>
2016-05-04 19:30:56 +0900

    doc: add comment to sdk.go

--- sdk.go
+++ sdk.go
@@ -14,9 +14,11 @@ type Client struct {
 	sendInterval time.Duration
 }
 
+// Global GoniPlus client
 var client *Client
 var metricURL = "goniplus.layer123.io:9900"
 
+// getIP() returns system's IP address in string
 func getIP() string {
 	addrs, err := net.InterfaceAddrs()
 	if err != nil {
@@ -32,10 +34,12 @@ func getIP() string {
 	return ""
 }
 
+// getPid() returns application's Pid in string
 func getPid() string {
 	return strconv.Itoa(os.Getpid())
 }
 
+// getInstance() returns instance id
 func getInstanceID() string {
 	return getIP() + "-" + getPid()
 }
