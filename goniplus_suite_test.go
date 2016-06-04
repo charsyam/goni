e8ff3ebd0d44099caa44af9959b1c183ade3ebd6
devholic <devholic@plusquare.com>
2016-06-05 01:08:15 +0900

    test: add test suite

---
+++ goniplus_suite_test.go
@@ -0,0 +1,13 @@
+package goniplus_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestGoniplusSdk(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "GoniplusSdk Suite")
+}
